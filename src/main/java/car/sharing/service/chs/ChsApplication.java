40f436f83a576ebc3f81df39f4914edbe95340de
Gosudar <Bohdan0410@gmail.com>
2026-03-22 17:46:37 +0200

    Initial commit

---
+++ src/main/java/car/sharing/service/chs/ChsApplication.java
@@ -0,0 +1,13 @@
+package car.sharing.service.chs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/car/sharing/service/chs/ChsApplicationTests.java
@@ -0,0 +1,13 @@
+package car.sharing.service.chs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
