5e0126357b49b268868c982d8f3afe8984c0037d
Gosudar <Bohdan0410@gmail.com>
2026-03-28 15:41:42 +0200

    create test application-test.properties

--- src/test/java/car/sharing/service/chs/ChsApplicationTests.java
+++ src/test/java/car/sharing/service/chs/ChsApplicationTests.java
@@ -1,12 +1,17 @@
 package car.sharing.service.chs;
 
+import car.sharing.service.chs.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ChsApplicationTests {
+
+    @MockBean
+    private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
